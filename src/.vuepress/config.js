d7281dce92bf9df6faea2f48b2c5d9f18cdcfb56
alextappin <alextappin@gmail.com>
2023-03-06 20:41:35 -0800

    playfair display

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -19,7 +19,7 @@ module.exports = {
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
-    ['link', { rel: 'stylesheet', href: '"https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap' }]
+    ['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap', type:"text/css" }]
     // < link rel = "apple-touch-icon" sizes = "180x180" href = "/apple-touch-icon.png" >
     // <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
     //   <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
